0efe36356e7a6a500ca4728d7a7b60f63177033c
Christopher Randin <cdrandin@hotmail.com>
2015-12-14 13:41:30 -0800

    Test for windows at command

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 
-from proj.tasks import *
+# from proj.tasks import *
+
+import os
 import datetime
 
 
 def main():
-    in_1_min = datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+    with open(os.path.dirname(os.path.realpath(__file__)) + 'file.txt', 'a+') as outfile:
+        outfile.write('[%s]: %s', datetime.datetime.now(), 'hello world!')
 
-    res = send_email.apply_async(
-        args=['cdrandin@hotmail.com', 'delayed message'], eta=in_1_min)
+    # in_1_min = datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+
+    # res = send_email.apply_async(
+    #     args=['cdrandin@hotmail.com', 'delayed message'], eta=in_1_min)
 
 if __name__ == '__main__':
     main()
